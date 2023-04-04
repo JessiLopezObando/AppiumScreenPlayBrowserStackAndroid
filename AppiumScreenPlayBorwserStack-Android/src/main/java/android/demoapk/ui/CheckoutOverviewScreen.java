520fbfefd47ab1013084be1a1ebd5d2996877261
SergioBalca <SergioBalca@github.com>
2023-04-04 18:20:01 -0500

    Se limpia el codigo de elementos que no se utilizan

--- AppiumScreenPlayBorwserStack-Android/src/main/java/android/demoapk/ui/CheckoutOverviewScreen.java
+++ AppiumScreenPlayBorwserStack-Android/src/main/java/android/demoapk/ui/CheckoutOverviewScreen.java
@@ -9,8 +9,6 @@ public class CheckoutOverviewScreen extends PageObject {
     public static final Target FINISH_BUTTON = Target.the("finish button")
             .located(By.xpath("//android.view.ViewGroup[@content-desc=\"test-FINISH\"]"));
 
-    public static final Target PAYMENT_INFORMATION_MESSAGE = Target.the("payment information")
-            .located(By.xpath("//android.widget.ScrollView[@content-desc=\"test-CHECKOUT: OVERVIEW\"]/android.view.ViewGroup/android.widget.TextView[1]"));
 
     public static final Target COMPLETE_ORDER_MESSAGE = Target.the("complete order message")
             .located(By.xpath("//android.widget.ScrollView[@content-desc=\"test-CHECKOUT: COMPLETE!\"]/android.view.ViewGroup/android.widget.TextView[1]"));
--- AppiumScreenPlayBorwserStack-Android/src/test/java/android/demoapk/setup/Configuracion.java
+++ AppiumScreenPlayBorwserStack-Android/src/test/java/android/demoapk/setup/Configuracion.java
@@ -1,9 +1,7 @@
 package android.demoapk.setup;
 
 import net.serenitybdd.screenplay.Actor;
-import net.thucydides.core.annotations.Managed;
 import org.apache.log4j.PropertyConfigurator;
-import org.openqa.selenium.WebDriver;
 
 import static android.demoapk.util.Log4j.LOG4J_PROPERTIES_FILE_PATH;
 import static com.google.common.base.StandardSystemProperty.USER_DIR;

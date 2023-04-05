11da85adade95a17d6b9b7a724c880a0560afdfc
SergioBalca <SergioBalca@github.com>
2023-04-05 11:27:45 -0500

    Se agrega los datos para diligenciar informacion compra al archivo .env

--- AppiumScreenPlayBorwserStack-Android/src/main/java/android/demoapk/util/Constants.java
+++ AppiumScreenPlayBorwserStack-Android/src/main/java/android/demoapk/util/Constants.java
@@ -5,8 +5,4 @@ public class Constants {
     public static final String PASSWORD = "secret_sauce";
     public static final String PRODUCTS = "PRODUCTS";
     public static final String MENSAJE_ORDEN_COMPLETA = "THANK YOU FOR YOU ORDER";
-
-    public static final String FIRST_NAME = "Andres";
-    public static final String LAST_NAME = "yepes";
-    public static final String ZIP_CODE = "1234";
 }
